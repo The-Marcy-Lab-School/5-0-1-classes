0888494c7b73f73f36daab5edb949580d0811112
Ben Spector <111444562+benspector-mls@users.noreply.github.com>
2025-02-12 15:45:59 -0500

    Update 0-factory.js

--- 0-factory.js
+++ 0-factory.js
@@ -7,12 +7,41 @@ const makeFriendsManager = (...initialFriends) => {
       return [...friends];
     },
     addFriend(newFriend) {
-      if (typeof newFriend !== 'string') return;
+      if (typeof newFriend !== 'string') {
+        throw Error('new friends must be strings');
+      };
       friends.push(newFriend);
     }
   }
 }
 
-// instances of the factory function
-const myFM = makeFriendsManager();
-const yourFM = makeFriendsManager();
+// instances of the factory function have their own friends
+const reuben = makeFriendsManager();
+reuben.addFriend('carmen')
+reuben.addFriend('ann')
+reuben.addFriend('motun')
+console.log(reuben.getFriends()); // ['carmen', 'ann', 'motun']
+
+
+const maya = makeFriendsManager();
+maya.addFriend('ben')
+maya.addFriend('gonzalo')
+console.log(maya.getFriends()); // ['ben', 'gonzalo']
+
+
+console.log(reuben);
+// {
+//   getFriends: [Function: getFriends],
+//   addFriend: [Function: addFriend]
+// }
+
+console.log(maya);
+// {
+//   getFriends: [Function: getFriends],
+//   addFriend: [Function: addFriend]
+// }
+
+// so, are they the same object?
+console.log(reuben === maya);
+// are the methods of these objects the same?
+console.log(reuben.addFriend === maya.addFriend);
